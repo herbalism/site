081d977349faedb84b91df1f7d8bbace8b61e4c0
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2012-12-11 00:00:48 +0100

    initial herbalism site

---
+++ js/index.js
@@ -0,0 +1,5 @@
+define(['foliage'], function(f) {
+    return f.div({'class': 'row'},
+	  f.div({'class': 'offset4 span4'},
+		f.p("hello world")))
+})
---
+++ loaderconf.js
@@ -0,0 +1,40 @@
+curl = {
+    baseUrl: 'js',
+    paths: {
+        'jquery': 'ext/jquery-1.8.2',
+        'jquery-hashchange': 'ext/jquery-hashchange',
+        'curl/plugin/twig': '../modules/twig/twig',
+        'curl/plugin/css': '../modules/curl/src/curl/plugin/css',
+	'bootstrap/css': '../modules/bootstrap/docs/assets/css'
+    },
+    packages: {
+	'lodash': {
+	    'location':'../modules/lodash',
+	    'main':'lodash'
+	},
+	'phloem': {
+	    'location':'../modules/phloem',
+            'main':'phloem'
+        },
+        'twig': {
+            'path':'../modules/twig',
+            'main':'twig',
+        },
+        'foliage' : {
+            'location':'../modules/foliage',
+            'main':'foliage',
+            'lib': 'ext'
+        },
+        'epiphyte' : {
+            'location':'../modules/epiphyte',
+            'main':'remoteAdapter',
+            'lib': 'ext'
+        },
+        'when' : {
+            'location': '../modules/when',
+            'main': 'when'
+        }
+    }
+};
+
+
